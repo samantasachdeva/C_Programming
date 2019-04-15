5766d45de0acf7dc4d30f4376715f3220cb584e6
samantasachdeva <38155710+samantasachdeva@users.noreply.github.com>
2019-04-15 19:33:17 +0530

    Create zero, until the user doesn't want to quit.c

---
+++ determine if a number is postive/negative/zero, until the user doesn't want to quit.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+  int n=1,i,ch;
+  while(n>0)
+  {
+    printf("enter the number\n");
+    scanf("%d",&i);
+    if(i>0)
+    printf("+ve");
+    else if(i<0)
+    printf("-ve");
+    else
+    printf("zero");
+    printf("\ndo you want to check again? enter 1 for yes and 0 for no");
+ 
+    scanf("%d",&ch);
+    
+    if(ch==1)
+    n++;
+    else if(ch==0)
+    {
+    n=0;
+    printf("see you soon!");
+    }
+  }
+
+
+
+  }
